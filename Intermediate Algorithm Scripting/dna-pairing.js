8d8756a8286a5052611362722d428778f363c5b3
Fatima Mahsud <fatimamahsud1@gmail.com>
2023-07-18 21:00:32 +0500

    Add files via upload

---
+++ Intermediate Algorithm Scripting/dna-pairing.js
@@ -0,0 +1,23 @@
+function pairElement(str) {
+  const matchWithBasePair = function(char) {
+    switch (char) {
+      case "A":
+        return ["A", "T"];
+      case "T":
+        return ["T", "A"];
+      case "C":
+        return ["C", "G"];
+      case "G":
+        return ["G", "C"];
+    }
+  };
+
+  const pairs = [];
+  for (let i = 0; i < str.length; i++) {
+    pairs.push(matchWithBasePair(str[i]));
+  }
+
+  return pairs;
+}
+
+pairElement("GCG");
